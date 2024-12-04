0aee63f4b1deb84fa2e6a0b2111a4fde667afcc2
Jay Van der Zant <amethi@gmail.com>
2024-12-04 11:20:12 +0000

    Experimenting with different PS module inclusions

--- Example Schedules/test-1.ps1
+++ Example Schedules/test-1.ps1
@@ -1,3 +1,9 @@
 $ErrorActionPreference = 'Stop'
-Import-Module LithnetRMA;
-Write-Host 'Hello World. This is test-1.ps1! LithnetRMA loaded.'
+$module_name = 'LithnetRMA'
+$module_name = 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\Modules\LithnetRMA\LithnetRMA.psd1'
+$module_name = 'TLS'
+$module_name = 'AppX'
+$module_name = 'LAPS'
+$module_name = 'DISM'
+Import-Module $module_name
+Write-Host "Hello World. This is test-1.ps1! $module_name module imported."
